22818ac6f83b429b4c662f73c08d7dffdab6b2ae
cnoss <cnoss@users.noreply.github.com>
2024-12-08 09:24:51 +0000

    deploy: 91840b7c3ec732b47ac176dd7b6f64afc005644e

--- src/_layouts/components/meta-data-head.11ty.js
+++
@@ -1,9 +0,0 @@
-exports.getHeader = ({ content }) => {
-
-
-  return `
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="author" content="Christian Noss">
-  `;
-};
--- src/mindmaps/mindmaps.11tydata.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  tags: ["mindmap"],
-  layout: "mindmap.11ty.js",
-};
